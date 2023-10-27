94d615dfbb8da9da5c630e5aa9fcc924cb930db7
GYEONGBIIN <138579795+GYEONGBIIN@users.noreply.github.com>
2023-10-27 09:24:35 +0900

    20231027

--- backend/login/views.py
+++ backend/login/views.py
@@ -1,6 +1,6 @@
 import re
 from django.contrib.auth import get_user_model
-from django.db import IntegrityError
+from .serializers import UserSerializer
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -10,27 +10,6 @@ from rest_framework import permissions
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ['id', 'username', 'password', 'email']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def validate_username(self, value):
-        if re.match(r'^[\w\.-]+@[\w\.-]+$', value):
-            raise serializers.ValidationError("이메일 형태는 아이디로 사용할 수 없습니다.", code='invalid_username')
-        return value
-
-    def create(self, validated_data):
-        try:
-            user = get_user_model().objects.create_user(
-                username=validated_data['username'],
-                email=validated_data['email'],
-                password=validated_data['password']
-            )
-            return user
-        except IntegrityError as e:
-            raise serializers.ValidationError("Username or email already exists.")
 
 class UserSignup(APIView):
     permission_classes = [permissions.AllowAny]
