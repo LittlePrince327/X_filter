e31453b9a1a6097d8fe9c3d0fae2fe6418b70eeb
GYEONGBIIN <138579795+GYEONGBIIN@users.noreply.github.com>
2023-11-07 19:05:11 +0900

    20231107

--- component/src/Detail/Detail.js
+++ component/src/Detail/Detail.js
@@ -5,14 +5,14 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:8000';
 
 const Detail = () => {
-    const postId = 123; // 실제 게시물 ID로 대체
-    const commentId = 456; // 실제 댓글 ID로 대체
+    const postId = 123;
+    const commentId = 456;
     const [userToken, setUserToken] = useState('');
     const [userData, setUserData] = useState(null);
 
     useEffect(() => {
         const tokenFromStorage = localStorage.getItem('token');
-    
+
         const fetchUserInfo = async () => {
             try {
                 const response = await axios.get(`http://localhost:8000/api/get_user_info/`, {
@@ -20,17 +20,17 @@ const Detail = () => {
                         Authorization: `Bearer ${tokenFromStorage}`,
                     },
                 });
-    
+
                 if (response.data.full_name) {
-                    localStorage.setItem('full_name', response.data.full_name); // Storing full_name in local storage
+                    localStorage.setItem('full_name', response.data.full_name);
                 }
-    
+
                 setUserData(response.data);
             } catch (error) {
-                console.error('Error fetching user information:', error);
+                console.error('사용자 정보를 불러오는 중 오류 발생:', error);
             }
         };
-    
+
         if (tokenFromStorage) {
             setUserToken(tokenFromStorage);
             fetchUserInfo();
@@ -40,96 +40,95 @@ const Detail = () => {
     const handlePostSubmit = (event) => {
         event.preventDefault();
         const content = event.target.content.value;
+        const author = localStorage.getItem('full_name'); // full_name 값을 author 변수에 할당
+        const create_date = new Date().toISOString(); // 현재 시간을 ISO 문자열로 변환하여 create_date에 할당
 
-        axios.post(`http://localhost:8000/board/xfilter/create/`, { content }, {
+        axios.post(`http://localhost:8000/board/xfilter/create/`, { content, author, create_date }, {
             headers: {
-                Authorization: `Bearer ${userToken}`, // userToken은 유저의 인증 토큰
+                Authorization: `Bearer ${userToken}`,
             },
         })
             .then(response => {
-                console.log('게시글이 성공적으로 생성되었습니다.', response);
-                // 추가적인 상태 업데이트 또는 사용자 경험 변경
+                console.log('게시물 제출 응답:', response);
             })
             .catch(error => {
-                console.error('게시글 생성 중 오류가 발생했습니다.', error);
+                console.error('게시물 제출 오류:', error);
             });
     };
-
     const handleCommentSubmit = (event) => {
         event.preventDefault();
         const content = event.target.content.value;
 
-        axios.post(`${BASE_URL}/board/comment/create/${postId}/`, { content }, {
+        axios.post(`http://localhost:8000/board/comment/create/${postId}/`, { content }, {
             headers: {
-                Authorization: `Bearer ${userToken}`, // userToken은 유저의 인증 토큰
+                Authorization: `Bearer ${userToken}`,
             },
         })
             .then(response => {
-                console.log('댓글이 성공적으로 생성되었습니다.', response);
-                // 추가적인 상태 업데이트 또는 사용자 경험 변경
+                console.log('댓글 제출 응답:', response);
             })
             .catch(error => {
-                console.error('댓글 생성 중 오류가 발생했습니다.', error);
+                console.error('댓글 제출 오류:', error);
             });
     };
 
     const handleEdit = (type, id) => {
-        const url = type === 'post' ? `${BASE_URL}/board/xfilter/modify/${id}/` : `${BASE_URL}/board/comment/modify/${id}/`;
+        const url = type === 'post' ? `http://localhost:8000/board/xfilter/modify/${id}/` : `http://localhost:8000/board/comment/modify/${id}/`;
 
         axios.post(url, null, {
             headers: {
-                Authorization: `Bearer ${userToken}`, // userToken은 유저의 인증 토큰
+                Authorization: `Bearer ${userToken}`,
             },
         })
             .then(response => {
                 const item = type === 'post' ? '게시물' : '댓글';
-                console.log(`${item}이 성공적으로 수정되었습니다.`, response);
+                console.log(`${item} 수정 응답:`, response);
             })
             .catch(error => {
                 const item = type === 'post' ? '게시물' : '댓글';
-                console.error(`${item} 수정 중 오류가 발생했습니다.`, error);
+                console.error(`${item} 수정 오류:`, error);
             });
     };
 
     const handleDelete = (type, id) => {
-        const url = type === 'post' ? `${BASE_URL}/board/xfilter/delete/${id}/` : `${BASE_URL}/board/comment/delete/${id}/`;
+        const url = type === 'post' ? `http://localhost:8000/board/xfilter/delete/${id}/` : `http://localhost:8000/board/comment/delete/${id}/`;
 
         axios.delete(url, {
             headers: {
-                Authorization: `Bearer ${userToken}`, // userToken은 유저의 인증 토큰
+                Authorization: `Bearer ${userToken}`,
             },
         })
             .then(response => {
                 const item = type === 'post' ? '게시물' : '댓글';
-                console.log(`${item}이 성공적으로 삭제되었습니다.`, response);
+                console.log(`${item} 삭제 응답:`, response);
             })
             .catch(error => {
                 const item = type === 'post' ? '게시물' : '댓글';
-                console.error(`${item} 삭제 중 오류가 발생했습니다.`, error);
+                console.error(`${item} 삭제 오류:`, error);
             });
     };
 
     const handleRecommend = (type, id) => {
-        const url = type === 'post' ? `${BASE_URL}/board/xfilter/vote/${id}/` : `${BASE_URL}/board/comment/vote/${id}/`;
+        const url = type === 'post' ? `http://localhost:8000//board/xfilter/vote/${id}/` : `http://localhost:8000/board/comment/vote/${id}/`;
 
         axios.post(url, null, {
             headers: {
-                Authorization: `Bearer ${userToken}`, // userToken은 유저의 인증 토큰
+                Authorization: `Bearer ${userToken}`,
             },
         })
             .then(response => {
                 const item = type === 'post' ? '게시물' : '댓글';
-                console.log(`${item}이 성공적으로 추천되었습니다.`, response);
+                console.log(`${item} 추천 응답:`, response);
             })
             .catch(error => {
                 const item = type === 'post' ? '게시물' : '댓글';
-                console.error(`${item} 추천 중 오류가 발생했습니다.`, error);
+                console.error(`${item} 추천 오류:`, error);
             });
     };
 
     return (
         <div className="container my-3" style={{ backgroundColor: 'white' }}>
-            <div className="top-section">
+            <div className="top section">
                 <form onSubmit={handlePostSubmit}>
                     <div>
                         <label htmlFor="content" className="form-label">게시글</label>
@@ -154,10 +153,10 @@ const Detail = () => {
             <div className="bottom-section">
                 <form onSubmit={handleCommentSubmit}>
                     <div>
-                        <label htmlFor="content" className="form-label">답변</label>
+                        <label htmlFor="content" className="form-label">label</label>
                         <textarea className="form-control" name="content" id="content" rows={10}></textarea>
                     </div>
-                    <button type="submit" className="btn btn-primary my-2">답변하기</button>
+                    <button type="submit" className="btn btn-primary my-2">댓글달기</button>
                     <button onClick={() => handleRecommend('comment', commentId)} className="btn btn-outline-success mx-2">
                         추천하기
                     </button>
