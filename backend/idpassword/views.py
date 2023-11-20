afeb5bf356d899c40de2ab2f9e17882799abdb51
GYEONGBIIN <138579795+GYEONGBIIN@users.noreply.github.com>
2023-11-20 17:02:44 +0900

    20231120

--- backend/idpassword/views.py
+++ backend/idpassword/views.py
@@ -1,13 +1,6 @@
 import json
 from django.http import HttpRequest, JsonResponse
-from django.shortcuts import render
-from django.urls import reverse
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth import get_user_model
-from django.contrib.auth.tokens import default_token_generator
-from django.contrib.auth.views import PasswordResetView
-from django.core.mail import send_mail
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
